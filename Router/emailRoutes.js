3d0b1b7f46692e29eb33474c8fcfd3e4314a3156
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-28 13:15:22 +0530

    Update emailRoutes.js

--- Router/emailRoutes.js
+++ Router/emailRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { forgetPassword, resetPassword } = require("../Controller/emailctrl");
+const { forgetPassword, resetPassword, updatePassword } = require("../Controller/emailctrl");
 
 // ✅ Create Device
 router.post("/forgetPassword", forgetPassword);  // ✅ Create Device
 
 router.post("/resetPassword", resetPassword);
+
+router.post("/updatePassword", updatePassword);
 
 module.exports = router;
