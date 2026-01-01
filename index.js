fcc3fb5616f367c6790b19e30df540e5426a47ed
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2026-01-01 17:02:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,6 @@ app.use((req, res, next) => {
 
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}..🖐🖐`);
 });
